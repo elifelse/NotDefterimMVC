eb03e96fac63c394a5a3510abbed9e0046d29b50
elifelse <elifgsavukcam@gmail.com>
2021-09-07 13:01:33 +0300

    V3 YeniNot

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NotDefterim.Data;
 using NotDefterim.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace NotDefterim.Controllers
@@ -12,15 +14,24 @@ namespace NotDefterim.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // giriş yapmış kullanıcı yok ise
+            if (!User.Identity.IsAuthenticated)
+                return View("IndexAnonymous");
+
+            // giriş yapmış kullanıcının id'si
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            return View(_db.Notes.Where(n => n.AuthorId == userId).ToList());
         }
 
         public IActionResult Privacy()
---
+++ Controllers/NotlarController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NotDefterim.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NotDefterim.Controllers
+{
+    [Authorize]
+    public class NotlarController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NotlarController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Yeni(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                note.AuthorId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                _db.Notes.Add(note);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+
+    internal class authorizeAttribute : Attribute
+    {
+    }
+}
