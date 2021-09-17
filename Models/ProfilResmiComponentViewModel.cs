7992163afc210c578dcc357d6feae3591c26f773
elifelse <elifgsavukcam@gmail.com>
2021-09-17 09:34:15 +0300

    V11 ViewComponent/Layout/adminProfilResmi

---
+++ Models/ProfilResmiComponentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotDefterim.Models
+{
+    public class ProfilResmiComponentViewModel
+    {
+        public string DosyaAd { get; set; }
+        public int Genislik { get; set; }
+        public int Yukseklik { get; set; }
+        public string Sinif { get; set; }
+
+    }
+}
---
+++ ViewComponents/ProfilResmiViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotDefterim.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using NotDefterim.Models;
+
+namespace NotDefterim.ViewComponents
+{
+    public class ProfilResmiViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ProfilResmiViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int genislik, int yukseklik, string sinif)
+        {
+            var userId = ((ClaimsPrincipal)User).FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+            var vm = new ProfilResmiComponentViewModel()
+            {
+                DosyaAd = user.Photo,
+                Genislik = genislik,
+                Yukseklik = yukseklik,
+                Sinif = sinif
+            };
+            return View(vm);
+        }
+    }
+}
